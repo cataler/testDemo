0ea97399ca38569f5428a8dcd8d88beb3d9e2c81
catal <cataler@163.com>
2016-07-12 11:45:51 +0800

    添加库文件

---
+++ testDemo/Lib/testLib.h
@@ -0,0 +1,13 @@
+//
+//  testLib.h
+//  testDemo
+//
+//  Created by CaiAntao on 16/7/12.
+//  Copyright © 2016年 CaiAntao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testLib : NSObject
+
+@end
---
+++ testDemo/Lib/testLib.m
@@ -0,0 +1,13 @@
+//
+//  testLib.m
+//  testDemo
+//
+//  Created by CaiAntao on 16/7/12.
+//  Copyright © 2016年 CaiAntao. All rights reserved.
+//
+
+#import "testLib.h"
+
+@implementation testLib
+
+@end
